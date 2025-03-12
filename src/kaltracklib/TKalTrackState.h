4a6682fb1303df9f2b26c2cb445071041cb9333b
sss <sss@karma>
2025-03-12 13:55:51 -0400

    Refresh of warn.KalTest

--- src/kaltracklib/TKalTrackState.h
+++ src/kaltracklib/TKalTrackState.h
@@ -60,8 +60,10 @@ public:
                            TKalMatrix	&F, 
                            TKalMatrix &Q) const;
 
-  using TKalMatrix::DebugPrint;
-  virtual void  DebugPrint(std::ostream&, Option_t *, Int_t  ) const { std::abort(); }
+  virtual void  DebugPrint(std::ostream& os, Option_t *, Int_t  ) const
+  { DebugPrint(os); }
+  virtual void  DebugPrint(Option_t *, Int_t  ) const
+  { DebugPrint(); }
   void         DebugPrint() const ;
   void         DebugPrint( std::ostream& os ) const;
 
